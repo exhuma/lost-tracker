fd48daaeba9a48bedd4075cfd56bb5c25ef91868
Michel Albert <michel@albert.lu>
2011-05-21 07:21:11 +0200

    First functional version

--- lost_tracker/main.py
+++ lost_tracker/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort
-from lost_tracker.models import Group, Station, get_state
+from flask import Flask, render_template, abort, jsonify
+from lost_tracker.models import Group, Station, get_state, advance as db_advance
 from lost_tracker.database import db_session as session
 app = Flask(__name__)
 
@@ -12,9 +12,17 @@ def index():
     for group in groups:
         tmp = [group]
         for station in stations:
-            tmp.append((group.id, station.id))
+            tmp.append(get_state(group.id, station.id))
         state_matrix.append(tmp)
     return render_template('matrix.html', matrix=state_matrix, stations=stations)
+
+@app.route('/advance/<groupId>/<station_id>')
+def advance(groupId, station_id):
+    new_state = db_advance(groupId, station_id)
+    return jsonify(
+            group_id=groupId,
+            station_id=station_id,
+            new_state=new_state)
 
 @app.route('/station/<base64key>')
 def station(base64key):
@@ -27,7 +35,7 @@ def station(base64key):
     groups = Group.query.all()
     return render_template('station.html',
             station=station,
-            group_states=[ (grp, get_state(grp, station)) for grp in groups ])
+            group_states=[ (grp, get_state(grp.id, station.id)) for grp in groups ])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- lost_tracker/models.py
+++ lost_tracker/models.py
@@ -15,14 +15,52 @@ group_station_state = Table(
         Column('state', Integer, default=STATE_UNKNOWN)
         )
 
-def get_state(group, station):
+def get_state(group_id, station_id):
     s = select(['state'], and_(
-        group_station_state.c.group_id == group.id,
-        group_station_state.c.station_id == station.id
+        group_station_state.c.group_id == group_id,
+        group_station_state.c.station_id == station_id
         ))
     s.bind = engine
     return list(s.execute())
 
+def advance(group_id, station_id):
+    s = select(['state'], and_(
+        group_station_state.c.group_id == group_id,
+        group_station_state.c.station_id == station_id
+        ))
+    s.bind = engine
+    result = list(s.execute())
+    if not result:
+        i = group_station_state.insert().values(
+                group_id=group_id,
+                station_id=station_id,
+                state = STATE_ARRIVED
+                )
+        i.bind=engine
+        i.execute()
+        return STATE_ARRIVED
+
+    try:
+        the_state = result[0][0]
+        if the_state == STATE_UNKNOWN:
+            new_state = STATE_ARRIVED
+        elif the_state == STATE_ARRIVED:
+            new_state = STATE_FINISHED
+        elif the_state == STATE_FINISHED:
+            new_state = STATE_UNKNOWN
+        upd = group_station_state.update().where(
+                and_(
+                    group_station_state.c.group_id == group_id,
+                    group_station_state.c.station_id == station_id)
+                ).values(
+                        state=new_state
+                        )
+        upd.bind = engine
+        upd.execute()
+        return new_state
+
+    except IndexError:
+        pass
 
 class Group(Base):
     __tablename__ = 'group'
