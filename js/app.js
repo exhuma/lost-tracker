f265e9aca3a8f41e3d737240ad9e97310f48d8c0
Michel Albert <michel@albert.lu>
2013-03-20 08:25:04 +0100

    Animations for group-divs started.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ goog.provide('tracker');
 goog.require('goog.array');
 goog.require('goog.dom');
 goog.require('lost_tracker.model.Station');
-
+goog.require('goog.fx.dom.ResizeWidth');
+goog.require('goog.style');
 
 /**
  * @constructor
@@ -14,10 +15,26 @@ var tracker = function() {
   var content = goog.dom.getElement('content');
 
   goog.array.forEach(groups, function(group) {
+    var ops = cd('div', {'class': 'ops'}, 'ops');
+    var groupinfo = cd('div', {'class': 'group'}, '' + group);
     var groupDiv = cd('div', {'class': 'group_widget'},
-      cd('div', {'class': 'ops'}, 'ops'),
-      cd('div', {'class': 'group'}, '' + group));
+      ops,
+      groupinfo);
     content.appendChild(groupDiv);
+    goog.events.listen(ops, goog.events.EventType.CLICK,
+      function(evt) {
+        var element = evt.target;
+        var parent = goog.dom.getParentElement(element);
+        var parsize = goog.style.getSize(parent);
+        window['console']['log'](parsize)
+        var elsize = goog.style.getSize(element);
+        var anim = new goog.fx.dom.ResizeWidth(element,
+          elsize.width,
+          parsize.width,
+          50);
+        goog.style.setStyle(groupinfo, 'display', 'none');
+        anim.play()
+      }, false, this);
   });
 
 };
