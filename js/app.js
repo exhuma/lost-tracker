e14b38c9e0d7e2de9f36d65811d076414112d44a
Michel Albert <michel@albert.lu>
2013-04-03 18:41:45 +0200

    Preliminary support to save the station score.
    Target URLs are still whacky. Just put them in so we have *somthing*...

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 goog.provide('lost_tracker.app');
 
+goog.require('goog.array');
 goog.require('goog.dom');
 goog.require('goog.dom.classlist');
+goog.require('goog.dom.forms');
+goog.require('goog.events');
+goog.require('goog.events.EventType');
 goog.require('goog.net.XhrIo');
 
 
@@ -36,6 +40,33 @@ lost_tracker.app.advanceState = function(event_source, groupId, stationId) {
 };
 
 
-goog.exportSymbol('lost_tracker.app', lost_tracker.app);
+/**
+ * Attach events to the form elements.
+ */
+lost_tracker.app.attachEvents = function(stationId) {
+  goog.array.forEach(goog.dom.getElementsByTagNameAndClass('div', 'group'),
+      function(element) {
+    var form = goog.dom.getElementsByTagNameAndClass('form', null, element);
+    var submit_buttons = goog.array.filter(form[0], function(felement) {
+      return felement.type == 'submit';
+    });
+    goog.events.listen(submit_buttons[0], goog.events.EventType.CLICK, function(evt) {
+      var formData = goog.dom.forms.getFormDataMap(evt.target.form);
+      goog.net.XhrIo.send(
+          '/group/' + formData.get('group_id')[0] + '/' + stationId,
+          function(evt){
+            var xhr = evt.target;
+            var data = xhr.getResponseJson();
+            window['console']['log'](data);
+          }, 'POST',
+          'score=' + formData.get('score')[0]);
+      evt.preventDefault();
+    });
+  });
+};
+
+
+goog.exportSymbol('lost_tracker.app.attachEvents',
+    lost_tracker.app.attachEvents);
 goog.exportSymbol('lost_tracker.app.advanceState',
     lost_tracker.app.advanceState);
