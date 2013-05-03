4b1cc56aea9bf450e63b9647633e8653dafaaa86
Michel Albert <michel@albert.lu>
2013-05-03 11:58:18 +0200

    Old "init_db" removed in favor of alembic

--- lost_tracker/database.py
+++ lost_tracker/database.py
@@ -5,14 +5,3 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False))
 Base = declarative_base()
 Base.query = db_session.query_property()
-
-
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-
-    import lost_tracker.models  # NOQA
-    import lost_tracker.main  # NOQA
-    Base.metadata.create_all()
-    print 'Database created on {0}'.format(Base.metadata)
