41b15d29f27917ad0ff0ece19d57d9d13652a3ed
Michel Albert <michel@albert.lu>
2013-05-08 18:59:45 +0200

    Version bump to 1.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
    name="lost-tracker",
-   version="1.2",
+   version="1.2.1",
    packages=find_packages(),
    install_requires=[
         'Flask==0.9',
