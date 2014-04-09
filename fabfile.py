1ccd0d3e8b9d30c3c48b4b77895102baca30c68c
Michel Albert <michel@albert.lu>
2014-04-09 08:19:44 +0200

    Deployment process uses a dedicated user now.

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,8 @@ import fabric.colors as clr
 
 
 fab.env.roledefs['prod'] = ['dozer.foobar.lu']
+REMOTE_USER = 'lostlu'
+REMOTE_FOLDER = '/var/www/lost.lu/beta'
 
 
 @fab.task
@@ -13,15 +15,14 @@ def deploy():
     fab.execute(build)
     fab.local('python setup.py sdist')
     name = fab.local('python setup.py --fullname', capture=True)
-    with fab.cd('/var/www/lost.lu/tracker'):
+    with fab.settings(user=REMOTE_USER), fab.cd(REMOTE_FOLDER):
         fab.put('dist/{0}.tar.gz'.format(name), '.')
         fab.run('env/bin/pip install {0}.tar.gz'.format(name))
         fab.put('alembic.ini', '.')
         fab.put('alembic', '.')
-        fab.run('LOST_TRACKER_SETTINGS=/var/www/lost.lu/tracker/siteconf.py '
-                'env/bin/alembic upgrade head')
+        fab.run('env/bin/alembic upgrade head')
         fab.run('touch wsgi/lost-tracker.wsgi')
-        #fab.run('rm {0}.tar.gz'.format(name))
+        fab.run('rm {0}.tar.gz'.format(name))
 
 
 @fab.task
