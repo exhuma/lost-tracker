exhuma/lost-tracker

commit e54b889e1738a6d83f19328dd24b8f552b0131da

Added an "admin" flag to users.

lost_tracker/main.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from config_resolver import Config
 from flask.ext.login import (
     LoginManager,
+    current_user,
     login_required,
     login_user,
     logout_user,
@@ -325,6 +326,8 @@ def register():
 @app.route('/confirm')
 @app.route('/confirm/<key>')
 def confirm_registration(key):
+    if not current_user.admin:
+        return "Access denied", 401
     loco.confirm_registration(
         key,
         activation_url=url_for('accept_registration',
@@ -339,6 +342,8 @@ def confirm_registration(key):
 @app.route('/accept/<key>')
 @login_required
 def accept_registration(key):
+    if not current_user.admin:
+        return "Access denied", 401
     group = loco.get_grp_by_registration_key(key)
 
     if group.finalized:
@@ -353,6 +358,8 @@ def accept_registration(key):
 @app.route('/group/<id>', methods=['POST'])
 @login_required
 def save_group_info(id):
+    if not current_user.admin:
+        return "Access denied", 401
     group = loco.get_grps_by_id(id)
     if not group.finalized:
         loco.accept_registration(group.confirmation_key, request.form)
@@ -396,6 +403,8 @@ def logout():
 @app.route('/manage')
 @login_required
 def manage():
+    if not current_user.admin:
+        return "Access denied", 401
     groups = loco.get_grps()
     slots = loco.slots()
 
@@ -421,6 +430,8 @@ def manage():
 @app.route('/manage/table/<table>')
 @login_required
 def tabularadmin(table):
+    if not current_user.admin:
+        return "Access denied", 401
 
     if table not in MODIFIABLE_TABLES:
         return gettext('Access Denied'), 401
@@ -481,6 +492,8 @@ def tabularadmin(table):
 @app.route('/cell/<cls>/<key>/<datum>', methods=['PUT'])
 @login_required
 def update_cell_value(cls, key, datum):
+    if not current_user.admin:
+        return "Access denied", 401
 
     if cls not in MODIFIABLE_TABLES:
         return gettext('Access Denied'), 401
@@ -528,6 +541,8 @@ def update_cell_value(cls, key, datum):
 @app.route('/group/<group_name>/timeslot', methods=['PUT'])
 @login_required
 def set_time_slot(group_name):
+    if not current_user.admin:
+        return "Access denied", 401
     data = request.json
     if data['direction'] not in (mdl.DIR_A, mdl.DIR_B):
         return jsonify(
@@ -552,6 +567,8 @@ def group_tooltip(group_id):
 @app.route('/station', methods=['POST'])
 @login_required
 def add_new_station():
+    if not current_user.admin:
+        return "Access denied", 401
     data = request.json
     message = loco.add_station(
         data['name'],
@@ -564,6 +581,8 @@ def add_new_station():
 @app.route('/form', methods=['POST'])
 @login_required
 def add_new_form():
+    if not current_user.admin:
+        return "Access denied", 401
     data = request.json
     name = data['name']
     max_score = int(data['max_score'])
@@ -580,6 +599,8 @@ def add_new_form():
 @app.route('/group', methods=['POST'])
 @login_required
 def add_new_group():
+    if not current_user.admin:
+        return "Access denied", 401
     data = request.json
     grp_name = data['name']
     grp_contact = data['contact']
@@ -600,6 +621,8 @@ def add_new_group():
 @app.route('/group/<int:id>', methods=['DELETE'])
 @login_required
 def delete_group(id):
+    if not current_user.admin:
+        return "Access denied", 401
     loco.delete_group(id)
     return jsonify(status='ok')
 
@@ -607,6 +630,8 @@ def delete_group(id):
 @app.route('/station/<int:id>', methods=['DELETE'])
 @login_required
 def delete_station(id):
+    if not current_user.admin:
+        return "Access denied", 401
     loco.delete_station(id)
     return jsonify(status='ok')
 
@@ -614,6 +639,8 @@ def delete_station(id):
 @app.route('/form/<int:id>', methods=['DELETE'])
 @login_required
 def delete_form(id):
+    if not current_user.admin:
+        return "Access denied", 401
     loco.delete_form(id)
     return jsonify(status='ok')
 

alembic/versions/8faadcbeda5_simple_auth.py
@@ -1,0 +1,22 @@
+"""simple-auth
+
+Revision ID: 8faadcbeda5
+Revises: 986ba0fc1c7
+Create Date: 2014-05-28 19:16:08.290579
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '8faadcbeda5'
+down_revision = '986ba0fc1c7'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('user', sa.Column('admin', sa.Boolean,
+                                    server_default='false'))
+
+def downgrade():
+    op.drop_column('user', 'admin')

lost_tracker/models.py
@@ -317,6 +317,7 @@ class User(Base):
     password = Column(Unicode(100))
     email = Column(Unicode(100))
     locale = Column(Unicode(2))
+    admin = Column(Boolean, default=False, server_default='false')
 
     def __init__(self, login, password, email):
         self.login = login

lost_tracker/templates/master.html
@@ -45,7 +45,7 @@
 
       <span id="auth">
         {% if current_user.is_authenticated() %}
-          {{_('Logged in as:')}} {{current_user.name}} |
+        {{_('Logged in as:')}} {{current_user.name}}{{'*' if current_user.admin else ''}} |
           <a href="{{url_for('group_list')}}">{{_('List')}}</a> |
           <a href="{{url_for('matrix')}}">{{_('Matrix')}}</a> |
           <a href="{{url_for('manage')}}">{{_('Slot Editor')}}</a> |

commit 7faee905d179391de5b79ea7f682493a0bb8d18a

Fixed regression introduced in e54b889e

lost_tracker/main.py
@@ -58,6 +58,10 @@ login_manager.login_view = "login"
 MODIFIABLE_TABLES = ('group', 'station', 'form')
 
 
+def userbool(value):
+    return value.lower()[0:1] in ('t', '1')
+
+
 def photo_url_generator(basename):
     return Photo(url_for('thumbnail', basename=basename),
                  url_for('photo', basename=basename))
@@ -129,8 +133,8 @@ def load_user(userid):
 
 @app.context_processor
 def inject_context():
-    registration_open = str(app.localconf.get(
-        'app', 'registration_open', default='f')).lower()[0:1] in ('t', '1')
+    registration_open = userbool(str(app.localconf.get(
+        'app', 'registration_open', default='f')))
     return dict(
         localconf=app.localconf,
         registration_open=registration_open,
@@ -345,7 +349,7 @@ def scoreboard():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     is_open = app.localconf.get('app', 'registration_open', default='False')
-    is_open = str(is_open).lower()[0:1] in ('t', '1')
+    is_open = userbool(str(is_open))
     if not is_open:
         return render_template('registration_closed.html')
 
@@ -381,8 +385,11 @@ def register():
 @app.route('/confirm')
 @app.route('/confirm/<key>')
 def confirm_registration(key):
-    if not current_user.admin:
+    is_open = app.localconf.get('app', 'registration_open', default='False')
+    is_open = userbool(str(is_open))
+    if not is_open:
         return "Access denied", 401
+
     loco.confirm_registration(
         key,
         activation_url=url_for('accept_registration',
@@ -397,7 +404,7 @@ def confirm_registration(key):
 @app.route('/accept/<key>')
 @login_required
 def accept_registration(key):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     group = loco.get_grp_by_registration_key(key)
 
@@ -413,7 +420,7 @@ def accept_registration(key):
 @app.route('/group/<id>', methods=['POST'])
 @login_required
 def save_group_info(id):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     group = loco.get_grps_by_id(id)
     if not group.finalized:
@@ -458,7 +465,7 @@ def logout():
 @app.route('/manage')
 @login_required
 def manage():
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     groups = loco.get_grps()
     slots = loco.slots()
@@ -485,7 +492,7 @@ def manage():
 @app.route('/manage/table/<table>')
 @login_required
 def tabularadmin(table):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
 
     if table not in MODIFIABLE_TABLES:
@@ -547,7 +554,7 @@ def tabularadmin(table):
 @app.route('/cell/<cls>/<key>/<datum>', methods=['PUT'])
 @login_required
 def update_cell_value(cls, key, datum):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
 
     if cls not in MODIFIABLE_TABLES:
@@ -595,7 +602,7 @@ def update_cell_value(cls, key, datum):
 @app.route('/group/<group_name>/timeslot', methods=['PUT'])
 @login_required
 def set_time_slot(group_name):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     data = request.json
     if data['direction'] not in (mdl.DIR_A, mdl.DIR_B):
@@ -621,7 +628,7 @@ def group_tooltip(group_id):
 @app.route('/station', methods=['POST'])
 @login_required
 def add_new_station():
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     data = request.json
     message = loco.add_station(
@@ -635,7 +642,7 @@ def add_new_station():
 @app.route('/form', methods=['POST'])
 @login_required
 def add_new_form():
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     data = request.json
     name = data['name']
@@ -653,7 +660,7 @@ def add_new_form():
 @app.route('/group', methods=['POST'])
 @login_required
 def add_new_group():
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     data = request.json
     grp_name = data['name']
@@ -675,7 +682,7 @@ def add_new_group():
 @app.route('/group/<int:id>', methods=['DELETE'])
 @login_required
 def delete_group(id):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     loco.delete_group(id)
     return jsonify(status='ok')
@@ -684,7 +691,7 @@ def delete_group(id):
 @app.route('/station/<int:id>', methods=['DELETE'])
 @login_required
 def delete_station(id):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     loco.delete_station(id)
     return jsonify(status='ok')
@@ -693,7 +700,7 @@ def delete_station(id):
 @app.route('/form/<int:id>', methods=['DELETE'])
 @login_required
 def delete_form(id):
-    if not current_user.admin:
+    if current_user.is_anonymous() or not current_user.admin:
         return "Access denied", 401
     loco.delete_form(id)
     return jsonify(status='ok')