8d413cf0195e950aa3c75314b5fc3b87acfa961a
Michel Albert <michel@albert.lu>
2016-03-31 19:11:03 +0200

    Added link to conversation to comment mail notifications.

--- lost_tracker/blueprint/comment.py
+++ lost_tracker/blueprint/comment.py
@@ -29,7 +29,8 @@ def add():
 
     group = mdl.Group.one(id=group_id)
 
-    if group not in current_user.groups:
+    if (group not in current_user.groups and
+            not current_user.has_role(mdl.Role.ADMIN)):
         return gettext('Access denied!'), 403
 
     content = request.form.get('content', '').strip()
@@ -38,7 +39,9 @@ def add():
                            current_app.mailer,
                            group,
                            current_user,
-                           content)
+                           content,
+                           url_for('group.show_comments', id=group_id,
+                                   _external=True))
 
     flash(gettext("Message saved!"))
     return redirect(url_for('group.show_comments', id=group_id))
